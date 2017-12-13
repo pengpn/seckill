6fa54fd6b72fbc9acdfce352d7a2a5fb91763e7d
pengpn <312924860@qq.com>
2017-12-13 17:25:38 +0800

    增加执行

---
+++ buy_mysql.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PENGPN
+ * Date: 2017/12/13
+ * Time: 17:22
+ */
+$url = 'http://seckill.app/index.php?app=app&c=seckill&a=addQsec&gid=2&type=mysql';
+$result = file_get_contents($url);
+
+var_dump($result);
