3c8b4b3af6ea4eda388d1201a7ecb26a1f418571
pengpn <312924860@qq.com>
2017-12-13 17:32:45 +0800

    加入帮助方法

--- Common/bootstrap.php
+++ Common/bootstrap.php
@@ -9,6 +9,7 @@
 
 defined('SEC_ROOT_PATH') or define('SEC_ROOT_PATH',dirname(dirname(__FILE__)));
 
+include 'function.php';
 include 'app/common.php';
 include 'Model/Model.php';
 include 'Model/GoodsModel.php';
